95b19d1c94d49479a818695656b6507778e6944c
Artem Shchirov <artemshchirov@gmail.com>
2023-03-12 00:08:01 +0200

    feat(api): SSR getStaticProps, getStaticPaths, interfaces and Course component and route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
+import axios from 'axios';
+import { GetStaticProps } from 'next';
 import { useState } from 'react';
 import { Button, Htag, Rating, Tag, Text } from '../components';
+import { MenuItem } from '../interfaces/menu.interface';
 import { withLayout } from '../layout/Layout';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
 	const [rating, setRating] = useState<number>(0);
 
 	return (
@@ -28,8 +31,35 @@ function Home(): JSX.Element {
 			</Tag>
 			<Tag size='sm'>Ghost</Tag>
 			<Rating rating={rating} setRating={setRating} isEditable></Rating>
+			<ul>
+				{menu.map((m: any) => (
+					<li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+				))}
+			</ul>
 		</>
 	);
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(
+		process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+		{
+			firstCategory,
+		}
+	);
+
+	return {
+		props: {
+			menu,
+			firstCategory,
+		},
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+}
